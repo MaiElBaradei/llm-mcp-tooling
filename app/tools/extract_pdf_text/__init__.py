6c74e598f2b3e34ffb1f8808aca911b2f845ffab
Mai Hassanin <maielbaradei@gmail.com>
2026-01-01 23:18:06 +0200

    Add ExtractPDFToolArgs class and update __all__ in extract_pdf_text module

--- app/tools/extract_pdf_text/__init__.py
+++ app/tools/extract_pdf_text/__init__.py
@@ -1,3 +1,4 @@
 from .extract_pdf_text import ExtractPDFTextTool
+from .extract_pdf_schema import ExtractPDFToolArgs
 
-__all__ = ["ExtractPDFTextTool"]
+__all__ = ["ExtractPDFTextTool", "ExtractPDFToolArgs"]
