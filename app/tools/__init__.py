d59cfac580164c018e5ca68761883040f5784e14
Mai Hassanin <maielbaradei@gmail.com>
2026-01-01 23:21:19 +0200

    Update __init__.py to include new tool arguments and remove GeminiClient

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -1,12 +1,26 @@
-from .extract_pdf_text import ExtractPDFTextTool
-from .summarize_pdf import SummarizePDFTool
-from .summarize_text import SummarizeTextTool, GeminiClient
-from .fetch_weather.fetch_weather_tool import FetchWeatherTool
-from .fetch_exchange_rate import FetchExchangeRateTool
+from .extract_pdf_text import ExtractPDFTextTool, ExtractPDFToolArgs
+from .summarize_pdf import SummarizePDFTool, SummarizePDFToolArgs
+from .summarize_text import SummarizeTextTool, SummarizeTextToolArgs
+from .fetch_weather import FetchWeatherTool, FetchWeatherToolArgs
+from .fetch_exchange_rate import FetchExchangeRateTool, FetchExchangeRateToolArgs
+from .detect_language import DetectLanguageTool, DetectLanguageToolArgs
+from .evaluate_llm import EvaluateLLMResponsesTool, EvaluateLLMResponsesToolArgs
+from .hallucination_checker import HallucinationCheckerTool, HallucinationCheckerToolArgs
+
 
 __all__ = ["ExtractPDFTextTool", 
            "SummarizePDFTool", 
            "SummarizeTextTool", 
-           "GeminiClient", 
            "FetchWeatherTool",
-           "FetchExchangeRateTool"]
+           "FetchExchangeRateTool",
+           "DetectLanguageTool",
+           "EvaluateLLMResponsesTool",
+           "HallucinationCheckerTool",
+           "ExtractPDFToolArgs",
+           "SummarizePDFToolArgs",
+           "SummarizeTextToolArgs",
+           "FetchWeatherToolArgs",
+           "FetchExchangeRateToolArgs",
+           "DetectLanguageToolArgs",
+           "EvaluateLLMResponsesToolArgs",
+           "HallucinationCheckerToolArgs"]
